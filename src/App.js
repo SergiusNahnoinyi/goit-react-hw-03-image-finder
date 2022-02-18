5800e4f8ae93173ff565135c9ca672ea82df4aeb
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-18 20:30:27 +0200

    Добавляет компонент ImageGallery в App
    Задал сразу проп imageName, который будет получать компонент ImageGallery со стейта App для запросов и рендеринга галлереи

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Searchbar from './components/Searchbar';
+import ImageGallery from './components/ImageGallery';
 
 import './App.css';
 
@@ -18,6 +19,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery imageName={this.state.imageName} />
         <ToastContainer />
       </div>
     );
