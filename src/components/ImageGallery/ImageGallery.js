46bfb3968b903e613e8d36506713bedde7439e53
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-18 20:50:25 +0200

    Реализует http-запросы при обновлении компонента
    Дополнительно реализована проверка на совпадение пред. и след. имени для предотвращения зацикливания компонента

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,9 +1,24 @@
 import s from './ImageGallery.module.css';
 import { Component } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem';
+import axios from 'axios';
 
 export default class ImageGallery extends Component {
   state = {};
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.imageName;
+    const nextName = this.props.imageName;
+
+    if (prevName !== nextName) {
+      axios
+        .get(
+          `https://pixabay.com/api/?q=${nextName}&page=1&key=24778312-18f63a423fbed9787418fdc16&image_type=photo&orientation=horizontal&per_page=12`,
+        )
+        .then(response => response.data)
+        .then(console.log);
+    }
+  }
 
   render() {
     return (
