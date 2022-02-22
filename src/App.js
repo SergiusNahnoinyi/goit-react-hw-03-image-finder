07a158783c7f4d269149ff1f75f0e031ec5849ce
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-22 15:56:17 +0200

    Добавляет комопнент Modal и метод для закрытия/открытия

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
+import Modal from './components/Modal';
 
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import './App.css';
 
 export default class App extends Component {
   state = {
     imageName: '',
+    showModal: false,
   };
 
   handleFormSubmit = query => {
     this.setState({ imageName: query });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
   };
 
   render() {
@@ -21,6 +30,7 @@ export default class App extends Component {
       <div className="App">
         <Searchbar onSubmit={this.handleFormSubmit} />
         <ImageGallery imageName={this.state.imageName} />
+        {this.state.showModal && <Modal onClose={this.toggleModal} />}
         <ToastContainer />
       </div>
     );
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,3 +1,11 @@
 import s from './Modal.module.css';
 
-export default function Modal() {}
+export default function Modal() {
+  return (
+    <div className={s.Overlay}>
+      <div className={s.Modal}>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
