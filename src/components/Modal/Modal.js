b14533856ef5f874754e97b6ac427fcdbb548111
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-22 16:05:22 +0200

    Создает портал для модалки

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,18 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
-export default function Modal() {
-  return (
-    <div className={s.Overlay}>
-      <div className={s.Modal}>
-        <img src="" alt="" />
-      </div>
-    </div>
-  );
+const modalRoot = document.querySelector('#modal-root');
+
+export default class Modal extends Component {
+  render() {
+    return createPortal(
+      <div className={s.Overlay}>
+        <div className={s.Modal}>
+          <img src="" alt="" />
+        </div>
+      </div>,
+      modalRoot,
+    );
+  }
 }
